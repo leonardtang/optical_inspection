3f336e0c631d26689e109360869e27f09d952fb8
leonardtang <leonardgentwintang@gmail.com>
2020-06-19 16:18:49 -0400

    adding dropout for real and ReLU (dumb)

--- cifar_10.py
+++ cifar_10.py
@@ -70,8 +70,6 @@ class SimpleCNN(torch.nn.Module):
 
         self.pool = torch.nn.MaxPool2d(kernel_size=2, stride=2, padding=0)  # --> (32, 16, 16)
 
-        self.dropOut = torch.nn.Dropout(p=0.5)
-
         self.conv2 = torch.nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1,
                                      padding=1)  # --> (64, 16, 16)
 
@@ -79,16 +77,12 @@ class SimpleCNN(torch.nn.Module):
 
         self.pool2 = torch.nn.MaxPool2d(kernel_size=2, stride=2, padding=0)  # --> (64, 8, 8)
 
-        self.dropOut = torch.nn.Dropout(p=0.5)
-
         self.conv3 = torch.nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1,
                                      padding=1)  # --> (128, 8, 8)
 
         self.batch3 = torch.nn.BatchNorm2d(128)
 
         self.pool3 = torch.nn.MaxPool2d(kernel_size=2, stride=2, padding=0)  # --> (128, 4, 4)
-
-        self.dropOut = torch.nn.Dropout(p=0.5)
 
         # Classification -- 18 in channels with 16 x 16 pixel-sized images = 4608 input nodes
         # 64 output nodes
@@ -106,24 +100,29 @@ class SimpleCNN(torch.nn.Module):
     def forward(self, x):
         # Pass through convolutional layer 1
         x = self.conv1(x)
-        x = self.batch1(x)
+        x = self.batch1(x)   # Normalize activations into non-linearity
         x = F.relu(x)
+        x = F.dropout(x, p=0.1)
         x = self.pool(x)
 
         x = self.conv2(x)
         x = self.batch2(x)
         x = F.relu(x)
+        x = F.dropout(x, p=0.1)
         x = self.pool2(x)
 
         x = self.conv3(x)
         x = self.batch3(x)
         x = F.relu(x)
+        x = F.dropout(x, p=0.1)
         x = self.pool3(x)
 
         # Need to flatten data from 18 channels to 4608 input nodes, i.e. (18, 16, 16) --> (1, 4608)
         x = x.view(-1, 128 * 4 * 4)
         x = self.fc1(x)
-        x = self.batch4(x)
+        x = self.batch4(x)   # Normalize activations into non-linearity
+        x = F.relu(x)
+        x = F.dropout(x, p=0.5)  # Drop out after non-linearity
 
         x = self.fc2(x)
         x = self.batch5(x)
